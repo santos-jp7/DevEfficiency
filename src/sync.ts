dd22b2fc5b626115fde3588987f765a4117413cf
Jean Pierre <hjepih@gmail.com>
2022-12-16 21:04:46 -0300

    feat (transactions): sync models com import

--- src/sync.ts
+++ src/sync.ts
@@ -1,12 +1,24 @@
 import fs from "fs";
 import path from "path";
 
-function syncModels() : void{
-    const files = fs.readdirSync(path.resolve("src", "models"));
+import Client from "./models/Client";
+import Credential from "./models/Credential";
+import Project from "./models/Project";
+import User from "./models/User";
 
-    files.forEach((modelName) => {
-        require(`./models/${modelName}`).default.sync();
-    });
+
+async function syncModels() : Promise<void>{
+    // const files = fs.readdirSync(path.resolve("src", "models"));
+
+    // files.forEach((modelName) => {
+    //     require(`./models/${modelName}`).default.sync({alter: true});
+    // });
+
+    await User.sync({alter: true});
+    await Client.sync({alter: true});
+    await Project.sync({alter: true});
+    await Credential.sync({alter: true});
+
 }
 
 export default syncModels;
