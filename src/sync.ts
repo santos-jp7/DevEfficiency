90045b52cf7357eb1e6009c422a2ab894c9c971e
santos-jp7 <hjepih@gmail.com>
2023-01-05 08:45:53 -0300

    feat (database sync): Sincronizando novas tabelas

--- src/sync.ts
+++ src/sync.ts
@@ -1,28 +1,32 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs'
+import path from 'path'
 
-import Client from "./models/Client";
-import Credential from "./models/Credential";
-import Project from "./models/Project";
-import Subproject from "./models/Subproject";
-import User from "./models/User";
-import ServiceOrder from "./models/ServiceOrder";
+import User from './models/User'
+import Client from './models/Client'
+import Credential from './models/Credential'
+import Project from './models/Project'
+import Subproject from './models/Subproject'
+import ServiceOrder from './models/ServiceOrder'
+import Protocol from './models/Protocol'
+import ProtocolRegister from './models/ProtocolRegister'
+import Received from './models/Received'
 
-
-async function syncModels() : Promise<void>{
+async function syncModels(): Promise<void> {
     // const files = fs.readdirSync(path.resolve("src", "models"));
 
     // files.forEach((modelName) => {
     //     require(`./models/${modelName}`).default.sync({alter: true});
     // });
 
-    await User.sync({alter: true});
-    await Client.sync({alter: true});
-    await Project.sync({alter: true});
-    await Subproject.sync({alter: true});
-    await ServiceOrder.sync({alter: true});
-    await Credential.sync({alter: true});
-
+    await User.sync({ alter: true })
+    await Client.sync({ alter: true })
+    await Project.sync({ alter: true })
+    await Subproject.sync({ alter: true })
+    await Credential.sync({ alter: true })
+    await ProtocolRegister.sync({ alter: true })
+    await Received.sync({ alter: true })
+    await Protocol.sync({ alter: true })
+    await ServiceOrder.sync({ alter: true })
 }
 
-export default syncModels;
+export default syncModels
