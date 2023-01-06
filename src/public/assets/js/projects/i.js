4142c50a81c4279569125da69df170ae066af83a
santos-jp7 <hjepih@gmail.com>
2023-01-06 17:43:46 -0300

    feat (ui): Projetos

---
+++ src/public/assets/js/projects/i.js
@@ -0,0 +1,49 @@
+let __api__ = null
+
+const project = new Vue({
+    el: '#project',
+    data: {
+        id: 0,
+        name: null,
+        Credentials: [],
+        Projects: [],
+    },
+    methods: {},
+    mounted: function () {
+        const token = localStorage.getItem('token')
+        const expires_in = localStorage.getItem('expires_in')
+        const type = localStorage.getItem('type')
+
+        if (!token || !expires_in || !type) return (location.href = './')
+        if (expires_in <= new Date().valueOf()) return (location.href = './')
+
+        const params = new Proxy(new URLSearchParams(window.location.search), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        })
+
+        __api__ = axios.create({
+            headers: {
+                Authorization: [type, token].join(' '),
+            },
+        })
+
+        __api__.get('/api/auth/verify').catch((error) => {
+            console.log(error)
+
+            localStorage.clear()
+
+            location.href = './'
+        })
+
+        __api__
+            .get('/api/projects/' + params.id)
+            .then(({ data }) => {
+                Object.keys(data).forEach((key) => (this.$data[key] = data[key]))
+            })
+            .catch((error) => {
+                console.log(error)
+
+                alert(error.response.data.message || 'Ocorreu um erro. Tente novamente mais tarde.')
+            })
+    },
+})
---
+++ src/public/assets/js/projects/projects.js
@@ -0,0 +1,49 @@
+let __api__ = null
+
+const projects = new Vue({
+    el: '#projects',
+    data: {
+        projectsOrigin: [],
+        projects: [],
+        q: null,
+    },
+    methods: {
+        handlerSearch() {
+            this.projects = this.projectsOrigin.filter((v) => v.name.includes(this.q))
+        },
+    },
+    mounted: function () {
+        const token = localStorage.getItem('token')
+        const expires_in = localStorage.getItem('expires_in')
+        const type = localStorage.getItem('type')
+
+        if (!token || !expires_in || !type) return (location.href = './')
+        if (expires_in <= new Date().valueOf()) return (location.href = './')
+
+        __api__ = axios.create({
+            headers: {
+                Authorization: [type, token].join(' '),
+            },
+        })
+
+        __api__.get('/api/auth/verify').catch((error) => {
+            console.log(error)
+
+            localStorage.clear()
+
+            location.href = './'
+        })
+
+        __api__
+            .get('/api/projects')
+            .then(({ data }) => {
+                this.$data.projectsOrigin = data
+                this.$data.projects = data
+            })
+            .catch((error) => {
+                console.log(error)
+
+                alert(error.response.data.message || 'Ocorreu um erro. Tente novamente mais tarde.')
+            })
+    },
+})
