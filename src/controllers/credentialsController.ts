9773dab6411ee958fa22762619547f7e82f14dde
santos-jp7 <hjepih@gmail.com>
2023-01-10 11:06:24 -0300

    feat (credentials): update e destroy

--- src/controllers/credentialsController.ts
+++ src/controllers/credentialsController.ts
@@ -25,18 +25,32 @@ class credentialsController {
         const credentials = await client?.createCredential({ description, host, username, password })
 
         return res.send(credentials)
-
-        // return res.send(await Credentials.create({
-        //     description, host, username, password, clientId
-        // }));
     }
 
-    static async update(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
-        return res.send('Hello World')
+    static async update(req: credentialsRequest, res: FastifyReply): Promise<FastifyReply> {
+        const { id } = req.params
+        const { description, host, username, password } = req.body
+
+        const credential = await Credential.findByPk(id)
+
+        await credential?.update({
+            description,
+            host,
+            username,
+            password,
+        })
+        await credential?.save()
+
+        return res.send(credential)
     }
 
-    static async destroy(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
-        return res.send('Hello World')
+    static async destroy(req: credentialsRequest, res: FastifyReply): Promise<FastifyReply> {
+        const { id } = req.params
+
+        const credential = await Credential.findByPk(id)
+        await credential?.destroy()
+
+        return res.status(204).send()
     }
 }
 
